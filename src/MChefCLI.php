d1a0bfdab57a1c253476f2700df87a9b8f909863
gthomas2 <dev@citri.city>
2026-01-22 11:42:39 +0000

    Issue #79: CR fix

--- src/MChefCLI.php
+++ src/MChefCLI.php
@@ -242,19 +242,26 @@ class MChefCLI extends CLI {
         if ($args = $options->getArgs()) {
             $recipe = $args[0];
 
-            if (file_exists($recipe)) {
+            // Resolve recipe path:
+            // - If an absolute path is provided, use it directly.
+            // - Otherwise, look for the recipe relative to the current working directory.
+            $cwd = getcwd();
+            $isAbsolute = !OS::isWindows() ?
+                (strlen($recipe) > 0 && ($recipe[0] === '/' || $recipe[0] === '\\')) // Unix or UNC-style
+                : preg_match('/^[A-Za-z]:[\\\\\\/]/', $recipe) === 1; // Windows drive letter
+            if ($isAbsolute) {
                 $recipePath = $recipe;
             } else {
                 // See if recipe is local to cwd.
-                $cwd = getcwd();
-                $recipePath = OS::path($cwd.'/'.$recipe);
-                if (!file_exists($recipePath)) {
-                    throw new \splitbrain\phpcli\Exception('Recipe file does not exist: ' . $recipe);
-                }
+                $recipePath = OS::path($cwd . '/' . $recipe);
+            }
+            if (!file_exists($recipePath)) {
+                throw new \splitbrain\phpcli\Exception('Recipe file does not exist: ' . $recipe);
             }
 
-            // Important - if we are upping a new recipe then we should temprarilly unset the currently selected instance.
+            // Important - if we are upping a new recipe then we should temporarily unset the currently selected instance.
             $configurator = \App\Service\Configurator::instance();
+
             $instance = $configurator->getMainConfig()->instance;
             $configurator->setMainConfigField('instance', null);
             $this->main->up($recipePath);
