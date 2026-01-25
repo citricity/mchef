7cc7b31854393bc857d8e2b5b833347597095502
gthomas2 <dev@citri.city>
2026-01-25 12:35:57 +0000

    Issue #91: Remove crontab command

--- assets/docker-init.sh
+++ assets/docker-init.sh
@@ -13,7 +13,6 @@ if [ "$ENABLE_MOODLE_CRON" = "true" ]; then
     # Create Moodle cron job
     echo "*/5 * * * * www-data /usr/local/bin/php $MOODLE_PATH/admin/cli/cron.php >/dev/null 2>&1" > /etc/cron.d/moodle-cron
     chmod 0644 /etc/cron.d/moodle-cron
-    crontab /etc/cron.d/moodle-cron
     echo "Moodle cron job configured"
 
     echo "Starting cron service..."
